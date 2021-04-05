9e7c37f2930afa58aff8018ef7c23088fc57fc87
Amaury Danis <amaury.danis-cousandier@epitech.eu>
2021-04-05 12:07:31 +0200

    simple ver done

--- src/main.c
+++ src/main.c
@@ -14,8 +14,8 @@ short get_cpu_tmp(void)
     //open the file with cpu temp
     fd = open("/sys/class/thermal/thermal_zone0/temp", O_RDONLY);
     read(fd, buff, 2);      //read only 2 first char of file content
-    close(fd);      //close da file
-    buff[2] = '\0';     //add end char at the end of string buff
+    close(fd);              //close da file
+    buff[2] = '\0';         //add end char at the end of string buff
     //return cpu temp converted to short var type
     return ((buff[0] - 48) * 10) + ((buff[1]) - 48);
 }
