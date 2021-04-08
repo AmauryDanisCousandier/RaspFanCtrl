136fb5f91a63a81b4f89172b7edf7e85df1dc586
Amaury Danis <amaury.danis-cousandier@epitech.eu>
2021-04-08 15:29:34 +0200

    simple ver done

--- src/main.c
+++ src/main.c
@@ -26,22 +26,43 @@ void wait_stime(int delay)
 	while (i++ != (delay * 100));
 }
 
-int main(int argc, char **argv)
+int init(void)
 {
-    short tmp;
-
-    if (!bcm2835_init())
-        return 1;
+    if (!bcm2835_init()) return 1;
     bcm2835_gpio_fsel(PIN, BCM2835_GPIO_FSEL_ALT5);
     bcm2835_pwm_set_clock(BCM2835_PWM_CLOCK_DIVIDER_16);
     bcm2835_pwm_set_mode(PWM_CHANNEL, 1, 1);
     bcm2835_pwm_set_range(PWM_CHANNEL, RANGE);
+    return 0;
+}
+
+int auto_mode(short low, short high)
+{
+    short tmp;
+
+    if (init()) return 1;
     while (1) {
         tmp = get_cpu_tmp();
-	if (tmp > 70)bcm2835_pwm_set_data(PWM_CHANNEL, 1023);
-        if (tmp < 60)bcm2835_pwm_set_data(PWM_CHANNEL, 0);
-	wait_stime(500);
+        if (tmp > high)bcm2835_pwm_set_data(PWM_CHANNEL, 1023);
+        if (tmp < low)bcm2835_pwm_set_data(PWM_CHANNEL, 0);
     }
     bcm2835_close();
+}
+
+int manual_mode(void)
+{
+    if (init()) return 1;
+    while (1) {
+        tmp = get_cpu_tmp();
+        if (tmp > high)bcm2835_pwm_set_data(PWM_CHANNEL, 1023);
+        if (tmp < low)bcm2835_pwm_set_data(PWM_CHANNEL, 0);
+    }
+    bcm2835_close();
+}
+
+int main(int argc, char **argv)
+{
+    if (argc == 1) return (auto_mode(60, 70));
+    if (argc == 2 && argv[1][0] == '-' && argv[1][1] == 'm') return (manual_mode());
     return 0;
 }
