a7b861f7602534b5a85b1b7345650b2cbd8843aa
amaury danis cousandier <amaury.danis-cousandier@epitech.eu>
2021-04-08 23:31:54 +0200

    other modes

--- src/main.c
+++ src/main.c
@@ -22,8 +22,8 @@ short get_cpu_tmp(void)
 
 void wait_stime(int delay)
 {
-	int i = 0;
-	while (i++ != (delay * 100));
+    int i = 0;
+    while (i++ != (delay * 100));
 }
 
 int init(void)
@@ -43,26 +43,40 @@ int auto_mode(short low, short high)
     if (init()) return 1;
     while (1) {
         tmp = get_cpu_tmp();
-        if (tmp > high)bcm2835_pwm_set_data(PWM_CHANNEL, 1023);
-        if (tmp < low)bcm2835_pwm_set_data(PWM_CHANNEL, 0);
+        if (tmp > high + 10)bcm2835_pwm_set_data(PWM_CHANNEL, 1023);
+        else if (tmp > high)bcm2835_pwm_set_data(PWM_CHANNEL, 800);
+        else if (tmp < low)bcm2835_pwm_set_data(PWM_CHANNEL, 0);
     }
     bcm2835_close();
 }
 
 int manual_mode(void)
 {
+    char buff;
+    int fan_speed = 500;
+
     if (init()) return 1;
     while (1) {
-        tmp = get_cpu_tmp();
-        if (tmp > high)bcm2835_pwm_set_data(PWM_CHANNEL, 1023);
-        if (tmp < low)bcm2835_pwm_set_data(PWM_CHANNEL, 0);
+        read(0, &buff, 1);
+        if (fan_speed >= 0 && buff == '+') fan_speed += 50;
+        else if (fan_speed <= 1000 && buff == '-') fan_speed -= 50;
+        bcm2835_pwm_set_data(PWM_CHANNEL, fan_speed);
     }
     bcm2835_close();
 }
 
+int static_mode(char *argv)
+{
+    short temp = ((argv[0] - 48) * 10) + ((argv[1]) - 48);
+    if (init()) return 1;
+    bcm2835_pwm_set_data(PWM_CHANNEL, temp);
+    return (0);
+}
+
 int main(int argc, char **argv)
 {
-    if (argc == 1) return (auto_mode(60, 70));
+    if (argc == 1) return (auto_mode(50, 60));
     if (argc == 2 && argv[1][0] == '-' && argv[1][1] == 'm') return (manual_mode());
+    if (argc == 2 && argv[1][0] == '-' && argv[1][1] == 's') return (static_mode(argv[2]));
     return 0;
 }
